5a12d3b96cedccc69fcbdaf395bdc1b2d6deda23
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-04-23 12:54:46 -0500

    se han agregado template autor y form autor

--- proyecto/gestion/forms.py
+++ proyecto/gestion/forms.py
@@ -6,6 +6,13 @@ class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
         fields = ['nombre', 'correo', 'nacionalidad', 'fecha_nacimiento', 'biografia']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'correo': forms.EmailInput(attrs={'class': 'form-control'}),
+            'nacionalidad': forms.TextInput(attrs={'class': 'form-control'}),
+            'fecha_nacimiento': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'biografia': forms.Textarea(attrs={'class': 'form-control'}),
+        }
 
 
 class LibroForm(forms.ModelForm):
--- proyecto/gestion/views.py
+++ proyecto/gestion/views.py
@@ -1,34 +1,48 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Autor, Libro
 from .forms import AutorForm, LibroForm
-# Create your views here.
 
-# CRUD Autores
+
 def lista_autores(request):
     autores = Autor.objects.all()
-    return render(request,'gestion/lista-autores.html',{'autores':autores})
+    return render(request, 'gestion/lista-autores.html', {'autores': autores})
+
+
 def crear_autor(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = AutorForm(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('lista_autores')
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autores')
+        else:
+            print(form.errors)  # 👈 para ver errores en consola
     else:
         form = AutorForm()
-    return render(request,'gestion/autor_form.html',{'form':form})
+
+    return render(request, 'gestion/autor_form.html', {'form': form})
+
+
 def editar_autor(request, pk):
     autor = get_object_or_404(Autor, pk=pk)
-    if request.method=='POST':
+
+    if request.method == 'POST':
         form = AutorForm(request.POST, instance=autor)
-    if form.is_valid():
-        form.save()
-        return redirect('lista_autores')
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autores')
+        else:
+            print(form.errors)
     else:
         form = AutorForm(instance=autor)
-    return render(request,'gestion/autor_form.html',{'form':form})
+
+    return render(request, 'gestion/autor_form.html', {'form': form})
+
+
 def eliminar_autor(request, pk):
     autor = get_object_or_404(Autor, pk=pk)
-    if request.method=='POST':
+
+    if request.method == 'POST':
         autor.delete()
         return redirect('lista_autores')
-    return render(request,'gestion/autor_confirm_delete.html',{'autor':autor})
+
+    return render(request, 'gestion/autor_confirm_delete.html', {'autor': autor})
